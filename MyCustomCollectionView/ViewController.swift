f26721f4f363d3aa58bb523a00a153f6bb418ecb
Yasutaka Kawamoto <kwmt27@gmail.com>
2014-12-30 13:25:13 +0900

    UICollectionView、UICollectionViewFlowLayout、UICollectionViewDataSourceの最低限の実装
    http://randexdev.com/2014/07/uicollectionview/

--- MyCustomCollectionView/ViewController.swift
+++ MyCustomCollectionView/ViewController.swift
@@ -5,18 +5,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UICollectionViewDelegateFlowLayout, UICollectionViewDataSource {
+  
+  var collectionView:UICollectionView?
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    let layout:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+    layout.sectionInset = UIEdgeInsets(top: 20, left: 10, bottom: 10, right: 10)
+    layout.itemSize = CGSize(width: 90, height: 120)
+    collectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: layout)
+    collectionView!.dataSource = self
+    collectionView!.delegate = self
+    collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
+    collectionView!.backgroundColor = UIColor.whiteColor()
+    self.view.addSubview(collectionView!)
+    
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
-
-
+  
+  //MARK: UICollectionViewDataSource Protocol
+  
+  //UICollectionViewDataSourceプロトコルは、
+  //collectionView:numberOfItemsInSection: メソッドと
+  //collectionView:cellForItemAtIndexPath: メソッドを必ず実装しなければならない
+  
+  //画面に表示するデータの個数を返す
+  func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 15
+  }
+  
+  //セルを返す
+  func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
+    cell.backgroundColor = UIColor.orangeColor()
+    
+    return cell
+  }
 }
 
